618d5c4e69d99b235b91f60f4d256d20af590087
mahmoudmostafa193 <shehatamahmoud400@gmail.com>
2025-04-02 03:48:32 +0200

    session6

---
+++ Company.G02.BLL/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Company.G02.BLL.Interfaces;
+using Company.G02.DAL.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.BLL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CompanyDbContext _context;
+        public IDepartmentRepository DepartmentRepository { get; }
+        public IEmployeeRepository EmployeeRepository { get; }
+
+        public UnitOfWork(CompanyDbContext companyDb, IDepartmentRepository departmentRepository, IEmployeeRepository employeeRepository)
+        {
+            DepartmentRepository = departmentRepository;
+            EmployeeRepository = employeeRepository;
+            _context = companyDb;
+
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public int Complete()
+        {
+           return _context.SaveChanges();
+        }
+    }
+}
---
+++ Company.G02.PL/Helpers/DocumentsSetting.cs
@@ -0,0 +1,39 @@
+﻿ namespace Company.G02.PL.Helpers
+{
+    public static class DocumentsSetting
+    {
+        public static string UploudFile(IFormFile file, string folderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\files", folderName);
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath); // تأكد من أن المجلد موجود
+            }
+
+            var fileExtension = Path.GetExtension(file.FileName); // استخراج الامتداد الصحيح
+            var fileName = $"{Guid.NewGuid()}{fileExtension}"; // اسم فريد مع الامتداد
+            var filePath = Path.Combine(folderPath, fileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return fileName; // ✅ الآن يتم إرجاع اسم الملف الصحيح
+        }
+
+
+
+        public static void DeleteFile(string fileName, string folderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\files", folderName);
+            var filePath = Path.Combine(folderPath, fileName);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+    }
+}
